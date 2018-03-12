fc643412ac62a1369a537efe26fb2a236701f800
chnoou <36837636+chnoou@users.noreply.github.com>
2018-03-12 09:19:26 +0100

    updated everyithing

---
+++ Protoshop/Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Protoshop.Models {
+    public class Products {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i namn!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i en beskrivning!")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Beskrivning mellan 10 och 500 karaktärer")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ett pris!")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Priset måste vara mellan 10 och 500 kr!")]
+        public double Price { get; set; }
+        [Required]
+        [Range(0,10000)]
+        public string ImageFile { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Protoshop/Models/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Protoshop.Models {
+    public class StoreContext {
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
